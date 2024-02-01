5019b8af8bea9daa9909dfc25ddceec66e1371db
josesantana.web <josesantana.web@gmail.com>
2024-02-01 17:40:47 -0400

    fix: section title

--- components/Vote/index.tsx
+++ components/Vote/index.tsx
@@ -1,19 +1,20 @@
 'use client'
 
-import Deposit from '@/components/Liquidity/Deposit'
+import Deposit from '@/components/Vote/Deposit'
 import Steps from '@/components/Common/Steps'
-import Pool from '@/components/Liquidity/Pool'
+import Pool from '@/components/Vote/Pool'
 
 import STEPS from './data'
 
 const Vote = () => {
   return (
     <section>
-      <div className="flex items-center gap-5">
+      {/* <div className="flex items-center gap-5">
         <Deposit />
         <Steps steps={STEPS} />
       </div>
-      <Pool />
+      <Pool /> */}
+      En Construccion....
     </section>
   )
 }
