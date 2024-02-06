62c70a6521303f7478889dca698a86c640fe3ce5
josesantana.web <josesantana.web@gmail.com>
2024-02-06 10:14:43 -0400

    feat: address migration

--- components/Claim/AddressCheck.tsx
+++ components/Claim/AddressCheck.tsx
@@ -14,29 +14,37 @@ const AddressCheck = ({ migrateStatus, setMigrateStatus }: AddressCheckProps) =>
   const handlerMigrateCheck = () => setMigrateStatus('wrong')
 
   return (
-    <div className="flex items-center gap-4 md:w-2/3 px-5 py-4 box-large">
-      <div className="flex items-center gap-5">
+    <div className="flex items-center gap-4 md:w-2/3 px-5 py-4 box-large flex-col md:flex-row">
+      <div className="flex items-center gap-5 md:flex-row">
         <p className="text-sm text-shark-100">Wallet Address</p>
-        <span className="px-3 py-1 text-sm border rounded-lg text-shark-100 bg-shark-400 border-shark-300">
+        <span className="px-3 py-1 text-xs md:text-sm border rounded-lg text-center text-shark-100 bg-shark-400 border-shark-300 truncate max-w-[200px]">
           {isConnected ? '0x878dfbs83fabc3x97d3469743d4E7' : '-'}
         </span>
       </div>
       {!migrateStatus && isConnected && (
-        <Button className="!py-2" onClick={handlerMigrateCheck}>
+        <Button className="!py-2 w-full md:w-auto" onClick={handlerMigrateCheck}>
           Check
         </Button>
       )}
       {migrateStatus === 'success' && (
         <div className="flex items-center gap-3">
-          <Button variant="tertiary" className="!py-2 !text-green-500 hover:!text-white" onClick={handlerMigrateCheck}>
+          <Button
+            variant="tertiary"
+            className="!py-2 !text-green-500 hover:!text-white w-[90%] md:w-auto"
+            onClick={handlerMigrateCheck}
+          >
             Check
           </Button>
           <span className="icon-check text-2xl text-green-500"></span>
         </div>
       )}
       {migrateStatus === 'wrong' && (
-        <div className="flex items-center gap-3">
-          <Button variant="tertiary" className="!py-2 !text-red-500 hover:!text-white" onClick={handlerMigrateCheck}>
+        <div className="flex items-center gap-3 w-full md:w-auto">
+          <Button
+            variant="tertiary"
+            className="!py-2 !text-red-500 hover:!text-white w-[90%] md:w-auto"
+            onClick={handlerMigrateCheck}
+          >
             Check
           </Button>
           <span className="icon-x-circle text-2xl text-red-500"></span>
--- components/Claim/TotalMigrated.tsx
+++ components/Claim/TotalMigrated.tsx
@@ -6,32 +6,32 @@ const TotalMigrated = () => {
     const isConnected = useStore((state) => state.isConnected)
 
   return (
-    <div className="relative flex items-center md:w-1/3 h-[62px] px-4 py-2 rounded-lg box-large gap-2">
-      <div className="flex items-center justify-center w-8 h-8 p-2 rounded-lg bg-shark-400 bg-opacity-60">
+    <div className="relative flex items-center md:w-1/3 md:h-[62px] px-4 py-2 rounded-lg box-large gap-2">
+      <div className="hidden md:flex items-center justify-center w-8 h-8 p-2 rounded-lg bg-shark-400 bg-opacity-60">
         <span
           className={`inline-block text-xl text-transparent bg-gradient-to-r from-outrageous-orange-500 to-festival-500 bg-clip-text icon-compass`}
         ></span>
       </div>
       <div className="">
-        <p className="text-xs text-shark-100">Total Migrated Amount to FNX</p>
-        <div className="flex items-center gap-2">
-          <div className="flex items-center gap-2">
+        <p className="text-xs text-shark-100 mb-3 md:mb-0">Total Migrated Amount to FNX</p>
+        <div className="flex items-center gap-3 md:gap-2 flex-wrap md:flex-nowrap">
+          <div className="flex items-center gap-2 w-full md:w-auto mb-2 md:mb-0 justify-between md:justify-start">
             <h3 className="text-sm text-white">0 CHR</h3>
             <span className="flex items-center justify-center w-5 h-5 text-xs text-white rounded-full icon-info bg-shark-200 hover:bg-outrageous-orange-500 cursor-pointer"></span>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 w-full md:w-auto mb-2 md:mb-0 justify-between md:justify-start">
             <h3 className="text-sm text-white">0 veCHR </h3>
             <span className="flex items-center justify-center w-5 h-5 text-xs text-white rounded-full icon-info bg-shark-200 hover:bg-outrageous-orange-500 cursor-pointer"></span>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 w-full md:w-auto mb-2 md:mb-0 justify-between md:justify-start">
             <h3 className="text-sm text-white">0 chrNFTs</h3>
             <span className="flex items-center justify-center w-5 h-5 text-xs text-white rounded-full icon-info bg-shark-200 hover:bg-outrageous-orange-500 cursor-pointer"></span>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 w-full md:w-auto mb-2 md:mb-0 justify-between md:justify-start">
             <h3 className="text-sm text-white">0 elCHR</h3>
             <span className="flex items-center justify-center w-5 h-5 text-xs text-white rounded-full icon-info bg-shark-200 hover:bg-outrageous-orange-500 cursor-pointer"></span>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 w-full md:w-auto mb-2 md:mb-0 justify-between md:justify-start">
             <h3 className="text-sm text-white">0 spCHR</h3>
             <span className="flex items-center justify-center w-5 h-5 text-xs text-white rounded-full icon-info bg-shark-200 hover:bg-outrageous-orange-500 cursor-pointer"></span>
           </div>
